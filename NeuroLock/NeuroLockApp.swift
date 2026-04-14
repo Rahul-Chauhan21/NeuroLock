0f149eb18a698d6bf07aab2a8d43b1ee530d9889
Rahul-Chauhan21 <crahul1721@gmail.com>
2026-04-15 03:17:19 +0530

    Initial Commit

---
+++ NeuroLock/NeuroLockApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuroLockApp.swift
+//  NeuroLock
+//
+//  Created by Rahul Chauhan on 4/15/26.
+//
+
+import SwiftUI
+
+@main
+struct NeuroLockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NeuroLockTests/NeuroLockTests.swift
@@ -0,0 +1,19 @@
+//
+//  NeuroLockTests.swift
+//  NeuroLockTests
+//
+//  Created by Rahul Chauhan on 4/15/26.
+//
+
+import Testing
+@testable import NeuroLock
+
+struct NeuroLockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
